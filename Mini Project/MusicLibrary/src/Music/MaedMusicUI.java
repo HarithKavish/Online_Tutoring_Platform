9ceb485dd6208328f10e7db3dc86140d4ef821b8
Harith Kavish S <94520772+HarithKavish@users.noreply.github.com>
2023-08-01 10:17:26 +0530

    Add files via upload

---
+++ Mini Project/MusicLibrary/src/Music/MaedMusicUI.java
@@ -0,0 +1,11 @@
+package Music;
+
+import java.util.Scanner;
+
+public interface MaedMusicUI {
+    void Welcome();
+    void Menu();
+    int Cont(Scanner sc);
+    int Close(Scanner sc);
+    int Regck(Scanner sc);
+}
